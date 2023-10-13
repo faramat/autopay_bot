a00a4eb99e34a54113bc7926f5da0887f75e2752
Adel Shakirov <del27-11@bk.ru>
2023-10-13 14:25:20 +0300

    new

---
+++ work_with_requests/yoomoney.py
@@ -0,0 +1,21 @@
+import requests,json
+
+
+
+
+url = 'https://yoomoney.ru/quickpay/confirm'
+client_id = '3F2B042CED54A545F1B23A7C9FABB9ACCB373F97D6BD7C7BC94F353EF581CCE9'
+headerds = {
+
+}
+params = {
+    'receiver':'4100116677873032',
+    'quickpay-form':'button',
+    'paymentType':'AC',
+    'sum':'10',
+    'label':'1234560',
+    'successURL':'https://google,com/'
+}
+response = requests.post(url,headers=headerds,params=params)
+print(response.url)
+
