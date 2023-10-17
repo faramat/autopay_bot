69bf208463cd5087b8724f7844e140e05ecbffad
Adelich27 <del27-11@bk.ru>
2023-10-18 01:47:35 +0300

    new

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from handlers import user_commands
 from config import Tokens,scheduler
 from data import db_start as db
 from callbacks import sub_channels,payments
-from misc.database import startup_notify
+from misc.database import startup_notify,check_subscription
 
 
 async def scheduler_start(bot):
-    scheduler.add_job(startup_notify, "interval", seconds = 10, args=(bot,))  # Ежедневный Автобэкап в 00:00
-    # scheduler.add_job(autobackup_admin, "cron", hour=12, args=(bot,))  # Ежедневный Автобэкап в 12:00
+    scheduler.add_job(check_subscription, "interval", seconds = 15, args=(bot,))  # Ежедневный Автобэкап в 00:00
+    # scheduler.add_job(startup_notify, "interval", hours = 12, args=(bot,))  # Ежедневный Автобэкап в 00:00
 
 bot = Bot(Tokens.bot_token,parse_mode="HTML")
 
@@ -23,6 +23,7 @@ async def main():
         payments.router
     )
     await startup_notify(bot)
+    await scheduler_start(bot)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
